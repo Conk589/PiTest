ee1294e24e94b59c3cca390a917721c62a187cf3
Cole <colemullenbach@gmail.com>
2025-01-10 23:08:41 -0500

    Errors with python i believe

--- gpioTests/buttonLED.py
+++ gpioTests/buttonLED.py
@@ -1,11 +1,12 @@
 from gpiozero import PWMLED
 from gpiozero import LED
 from gpiozero import Button
+from signal import pause
 from time import sleep
 
 light = PWMLED(17)
 button = Button(2)
 
+light.pulse
 
-while True:
-    light.pulse
+pause()
