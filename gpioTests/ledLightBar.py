df8e32062e35b4917d7854b3db622b88d1f4afa0
Cole <colemullenbach@gmail.com>
2025-01-11 18:11:54 -0500

    First try ledLightBar

--- gpioTests/ledLightBar.py
+++ gpioTests/ledLightBar.py
@@ -1,6 +1,7 @@
 #This light bar needs 220 Ohm Resistors between 3.3V and GPIO pins
 #it also, inconvieniently, only works one direction and is labeled randomly so turn it around if no worky
 #works by making an array of lights and changing modes based on a button press
+from time import sleep
 from gpiozero import LED
 from gpiozero import Button
 from signal import pause
@@ -11,6 +12,11 @@ button = Button(21)
 
 ref = 0
 
+def incrementRef():
+    global ref
+    ref += 1
+    if (ref > 12):
+        ref = 0
 
 def waterfall():
     while True:
@@ -24,16 +30,36 @@ def waterfall():
             lights[index].on() 
 
 
-def lightChange():
+def step():
     while True:
-        button.wait_for_active
-        ref = ref + 1
-        
+        for index in range(0,len(lightPins),1):
+            lights[index].on()
+            sleep (0.1)
+            lights[index].off()
 
-        
-        
-            
+def singles():
+    
+    lights[ref].on()
+    button.when_activated = lights[ref - 1].off
+    pause()
 
-button.when_activated = 
+    
 
-pause()
+
+if __name__ == "__main__":
+
+    button.when_activated = incrementRef
+
+    if (ref >= 10):
+        singles()
+    elif (ref == 11):
+        step()
+    elif (ref == 12):
+        waterfall()
+    
+    pause()
+
+
+
+#button.when_pressed = incrementRef
+
